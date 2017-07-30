a804d8484bb463ac36d91b6f303026265414802c
Mark Deazley <mdeazley@gmail.com>
2017-07-29 18:55:24 -0600

    POM Version 1.0.0
    Fixed test mode java.awt.headless.

--- src/test/java/com/alphapulsar/sb01/Sb01ApplicationTests.java
+++ src/test/java/com/alphapulsar/sb01/Sb01ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.alphapulsar.sb01;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Sb01ApplicationTests {
+
+	@BeforeClass
+	public static void before() {
+		System.setProperty("java.awt.headless", "false");
+	}
 
 	@Test
 	public void contextLoads() {
